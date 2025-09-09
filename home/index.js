b2c0a752c147df9e12a3ccc792357187b2a2e37d
bakti0856 <baktiprimayuda@gmail.com>
2025-09-09 16:27:58 +0700

    update

--- home/index.js
+++ home/index.js
@@ -32,3 +32,18 @@
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
   });
+
+    window.addEventListener('scroll', reveal);
+    function reveal() {
+      const reveals = document.querySelectorAll('.visible');
+      for (let i = 0; i < reveals.length; i++) {
+        const windowHeight = window.innerHeight;
+        const elementTop = reveals[i].getBoundingClientRect().top;
+        const elementVisible = 100;
+        if (elementTop < windowHeight - elementVisible) {
+          reveals[i].classList.add('active');
+        } else {
+          reveals[i].classList.remove('active');
+        }
+      }
+    }
